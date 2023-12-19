124874c88b4954421aee07c28d8185c278973b73
Lorris <97036825+lorris-sanna@users.noreply.github.com>
2023-12-19 16:27:39 +0100

    Add files via upload

---
+++ main.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+#include"terrain.h"
+#include "personnage.h"
+#include"monstre.h"
+
+using namespace std;
+
+void test1()
+{
+    terrain t{0,0};
+    t.initialiserTerrain();
+    t.definirTerrain();
+    t.afficherTerrain();
+    monstre m{50,80,3,2,10};
+    t.placerPersonnage(m,5,3);
+}
+
+//main mis en commentaire pour éviter redéclaration à cause de maintest.cpp
+/*int main()
+{
+   /* terrain t{5,7};
+    t.genererTerrain();
+    t.afficherTerrain();*/
+   /*test1();
+   return 0;
+}*/
